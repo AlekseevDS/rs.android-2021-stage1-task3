0a7760eb8f8e1f0a2ac6fb7acde1c69951d6d682
AlekseevDS <alekseevdms@gmail.com>
2021-07-04 18:47:51 +0300

    feat: solving task1

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,24 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        var xForReturn: Int? = null
+
+        for (i in 1..array[1]) {
+            val binom = factorial(array[1])/(factorial(array[1] - i) * factorial(i))
+            if (binom >= array[0]) {
+                xForReturn = i
+                break
+            }
+        }
+        return xForReturn
+    }
+
+    private fun factorial(num: Int): Long {
+        var result:Long = 1L
+        for (i in 1..num) {
+            result *= i
+        }
+        return result
     }
 }
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,25 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
         throw NotImplementedError("Not implemented")
+        val sqNum = number * number
+        var elemOfSequence = 0
+        var arrElemOfSequence: ArrayList<Int>
+        val firstElemOfSequence = number - 1
+        arrElemOfSequence.add(firstElemOfSequence * firstElemOfSequence)
+        val tempOfposition = 0
+
+        while (sqNum > arrElemOfSequence.sum()) {
+            var ostatok = sqNum - arrElemOfSequence.sum()
+            elemOfSequence = sqrt(ostatok.toDouble()).toInt()
+            arrElemOfSequence.add(elemOfSequence * elemOfSequence)
+        }
+
+
     }
 }
